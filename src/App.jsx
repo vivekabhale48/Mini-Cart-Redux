e3fb83037f05534ca4a3cfdcd2aa0a3152d4ba94
vivekabhale48 <vivek.abhale48@gmail.com>
2024-12-24 17:17:48 +0530

    Added new component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { products } from './utils/constants';
 import { useDispatch, useSelector } from 'react-redux';
 import { addItemToCart } from './store/slices/cartSlice';
 import { FaRupeeSign } from 'react-icons/fa';
+import { CartSection } from './components/CartSection';
 
 function App() {
   const dispatch = useDispatch();
@@ -61,74 +62,7 @@ function App() {
 
       <div className="cart-section mt-5 px-5 py-2">
         {items.length > 0 && (
-          <div className="flex gap-3 bg-slate-100">
-            <div className="p-2">
-              <div className="flex items-center justify-between">
-                <h3 className="text-xl font-bold">Products added to Cart</h3>
-                <h3 className="text-xl font-bold">{items.length} Items</h3>
-              </div>
-              <hr className="mt-5 mb-5 text-slate-800" />
-              <div>
-                <table className="border border-collapse w-full border-gray-200">
-                  <thead>
-                    <tr className="bg-gray-100">
-                      <th className="border border-gray-300 px-4 py-2 w-2/4 uppercase">
-                        Product Details
-                      </th>
-                      <th className="border border-gray-300 px-4 py-2 w-1/6 uppercase">
-                        Quantity
-                      </th>
-                      <th className="border border-gray-300 px-4 py-2 w-1/6 uppercase">
-                        Price
-                      </th>
-                      <th className="border border-gray-300 px-4 py-2 w-1/6 uppercase">
-                        Total
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {items.map((ele, index) => (
-                      <tr key={index}>
-                        <td className="p-2">
-                          <span className="ml-4 font-[600] text-blue-800 text-[18px]">
-                            {ele.name}
-                          </span>
-                        </td>
-                        <td className="p-2 text-center">
-                          <span className="font-[600] text-blue-800 text-[18px]">
-                            {ele.quantity}
-                          </span>
-                        </td>
-                        <td className="p-2 text-center">
-                          <span className="font-[600] text-blue-800 text-[18px]">
-                            {ele.price}
-                          </span>
-                        </td>
-                        <td className="p-2 text-center">
-                          <span className="font-[600] text-blue-800 text-[18px]">
-                            {ele.quantity * ele.price}
-                          </span>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-
-            <div className="bg-[#CBC3E3] w-full">
-              <div>
-                <h3 className="text-xl font-bold mt-2 px-2">Order Summary</h3>
-                <hr className="mt-5 mb-5 border-gray-500 mx-2" />
-              </div>
-              <div className="text-[16px] px-2 font-bold flex justify-between items-center">
-                <span>ITEMS {items.length}</span>
-                <span className="flex items-center justify-center">
-                  {totalValue} <FaRupeeSign />
-                </span>
-              </div>
-            </div>
-          </div>
+            <CartSection items={items} totalValue={totalValue}/>
         )}
       </div>
     </div>
---
+++ src/components/CartSection.jsx
@@ -0,0 +1,75 @@
+import { FaRupeeSign } from "react-icons/fa"
+
+export const CartSection = ({ items, totalValue }) => {
+
+    return (
+        <div className="flex gap-3 bg-slate-100">
+            <div className="p-2 flex-1">
+                <div className="flex items-center justify-between">
+                    <h3 className="text-xl font-bold">Products added to Cart</h3>
+                    <h3 className="text-xl font-bold">{items.length} Items</h3>
+                </div>
+                <hr className="mt-5 mb-5 text-slate-800" />
+                <div>
+                    <table className="border border-collapse w-full border-gray-200">
+                        <thead>
+                            <tr className="bg-gray-100">
+                                <th className="border border-gray-300 px-4 py-2 w-2/4 uppercase">
+                                    Product Details
+                                </th>
+                                <th className="border border-gray-300 px-4 py-2 w-1/6 uppercase">
+                                    Quantity
+                                </th>
+                                <th className="border border-gray-300 px-4 py-2 w-1/6 uppercase">
+                                    Price
+                                </th>
+                                <th className="border border-gray-300 px-4 py-2 w-1/6 uppercase">
+                                    Total
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {items.map((ele, index) => (
+                                <tr key={index}>
+                                    <td className="p-2">
+                                        <span className="ml-4 font-[600] text-blue-800 text-[18px]">
+                                            {ele.name}
+                                        </span>
+                                    </td>
+                                    <td className="p-2 text-center">
+                                        <span className="font-[600] text-blue-800 text-[18px]">
+                                            {ele.quantity}
+                                        </span>
+                                    </td>
+                                    <td className="p-2 text-center">
+                                        <span className="font-[600] text-blue-800 text-[18px]">
+                                            {ele.price}
+                                        </span>
+                                    </td>
+                                    <td className="p-2 text-center">
+                                        <span className="font-[600] text-blue-800 text-[18px]">
+                                            {ele.quantity * ele.price}
+                                        </span>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div className="bg-[#CBC3E3] flex-1">
+                <div>
+                    <h3 className="text-xl font-bold mt-2 px-2">Order Summary</h3>
+                    <hr className="mt-5 mb-5 border-gray-500 mx-2" />
+                </div>
+                <div className="text-[16px] px-2 font-bold flex justify-between items-center">
+                    <span>ITEMS {items.length}</span>
+                    <span className="flex items-center justify-center">
+                        {totalValue} <FaRupeeSign />
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
